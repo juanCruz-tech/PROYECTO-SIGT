60c7d1afdf81da261e190649c1339327501ec3cb
Esteban <juancruzborja7890@gmail.com>
2025-12-16 17:21:37 -0500

    Agrego la vista que tiene el administrador al momento de visualizar la cantidad de pijamas que hay para la venta

---
+++ Trimestre5/SIGT_FRONT/REACT SIN CONEXION/Inventario.jsx
@@ -0,0 +1,25 @@
+import InventarioPage from "../components/components admin/InventarioPage";
+import HeaderLine from "../components/HeaderLine";
+import FooterLine from "../components/FooterLine";
+import "../styles/styleAdminEmpleados.css";
+
+export default function Inventario (){
+    return (
+<>
+
+   <HeaderLine/>
+   <InventarioPage/>
+   <FooterLine/>
+
+
+</>
+
+
+
+    );
+
+
+    
+
+
+}
